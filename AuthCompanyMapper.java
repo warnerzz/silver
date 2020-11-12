b6fc82f9f69f235fe199a9380d7bd3ac382599c8
warnerzz <63141861+warnerzz@users.noreply.github.com>
2020-11-12 14:21:04 +0800

    Create AuthCompanyMapper.java
    AuthCompanyMapper

---
+++ AuthCompanyMapper.java
@@ -0,0 +1,25 @@
+package com.plm.dao.auth;
+
+import com.plm.model.Page;
+import com.plm.model.auth.AuthCompany;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+public interface AuthCompanyMapper {
+    int deleteByPrimaryKey(Long id);
+
+    int insert(AuthCompany record);
+
+    int insertSelective(AuthCompany record);
+
+    AuthCompany selectByPrimaryKey(Long id);
+
+    int updateByPrimaryKeySelective(AuthCompany record);
+
+    int updateByPrimaryKey(AuthCompany record);
+
+    List<AuthCompany> selectParams(@Param("company")  AuthCompany company);
+
+    List<AuthCompany> selectParamsPage(@Param("page") Page page, @Param("company")  AuthCompany company);
+}
